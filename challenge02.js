a23ed299d995ad04cea25d2681669d7b8f2711cb
Frank Langanke <franklanganke>
2015-03-26 16:06:42 +0100

    seven out of ten solution

--- challenge02.js
+++ challenge02.js
@@ -2,8 +2,8 @@
     init: function(elevators, floors) {
         var elevator = elevators[0]; // Let's use the first elevator
 
-        elevator.on("idle", function() {
-            // nothing to do
+        elevator.on("floor_button_pressed", function(floorNum) {
+            elevator.goToFloor(floorNum, (elevator.loadFactor() >= 0.6));
         });
         
         floors.forEach( function(f) { 
@@ -15,6 +15,21 @@
                 elevator.goToFloor(f.floorNum());
             });
         });
+
+        // Remove current floor form queue
+        elevator.on("stopped_at_floor", function(floorNum) {
+            var index = elevator.destinationQueue.indexOf(floorNum);
+            if (index > -1) {
+                elevator.destinationQueue.splice(index, 1);
+                elevator.destinationQueue.splice(index, 1);
+                elevator.destinationQueue.splice(index, 1);
+                elevator.destinationQueue.splice(index, 1);
+                elevator.destinationQueue.splice(index, 1);
+                elevator.destinationQueue.splice(index, 1);
+            }
+            elevator.checkDestinationQueue();
+        })        
+        
     },
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
